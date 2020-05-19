c960c2ece20db6de3301fc33a3022a68db8d631b
ZweekQ@163c.om <ZweekQ@163c.om>
2020-05-19 16:24:00 +0800

    第三版本

--- src/cn/core/ClientHandler.java
+++ src/cn/core/ClientHandler.java
@@ -6,6 +6,7 @@ import cn.http.HTTPResponse;
 import java.io.*;
 import java.net.Socket;
 import java.util.PrimitiveIterator;
+import cn.webService.webContext;
 
 /**
  * 这个类用来完成webserver的抽取过程
@@ -25,19 +26,28 @@ public class ClientHandler implements Runnable{
         //获取数据响应
         try {
 
-            //获取并处理请求
+            //获取服务端发来的数据,并处理请求.getInputStream方法可以得到一个输入流，客户端的Socket对象上的getInputStream方法得到输入流其实就是从服务器端发回的数据
             HTTPRequest request = new HTTPRequest(socket.getInputStream());
 
-            //利用httpResponse对浏览器做出数据响应
+            //利用httpResponse对浏览器做出数据响应,getOutputStream方法得到的是一个输出流，客户端的Socket对象上的getOutputStream方法得到的输出流其实就是发送给服务器端的数据
             HTTPResponse response = new HTTPResponse(socket.getOutputStream());
 
             //响应文件的地址
-            File file = new File("C:\\NewFile\\web"+ request.getUrl());
+            File file = new File(webContext.webRoot + "/" + request.getUrl());
+
+            //判断访问的文件是否存在
+            if (file.exists()) {
+                response.setStatus(200);
+            }else{
+                //文件不存在响应404文件
+                file = new File(webContext.webRoot + "/" + webContext.notFoundPage);
+                response.setStatus(404);
+            }
 
             //设置参数
-            response.setProtocol("HTTP/1.1");
-            response.setStatus(200);
-            response.setContentType("text/html");
+            response.setProtocol(webContext.procotol);
+            //动态获取响应内容的类型
+            response.setContentType(textType(request.getUrl()));
             response.setContentLength((int)file.length());
 
             responseFile(response,file);
@@ -46,6 +56,13 @@ public class ClientHandler implements Runnable{
             e.printStackTrace();
         }
     }
+
+    private String textType(String url) {
+        //截取url的后缀名
+        String lastStirng = url.substring(url.lastIndexOf(".") + 1);
+        return webContext.map.get(lastStirng);
+    }
+
 
     public void responseFile(HTTPResponse response,File file){
         //4.响应实体
--- src/cn/core/WebServer.java
+++ src/cn/core/WebServer.java
@@ -4,14 +4,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+import cn.webService.webContext;
 /**
  * 这个类用来代表服务器端
  * 1.申明ServerSocket对象
  * 2.在构造函数中初始化ServerSocket对象
- * 3.创建start方法，用来接收客户端请求和响应
+ * 3.创建start方法，用来接收客户端请求和并做出响应
  * 4。创建main方法启动服务器
- * 5.测试
  */
 public class WebServer {
     //1.申明ServerSocket对象
@@ -23,11 +22,11 @@ public class WebServer {
     public WebServer(){
 
         try{
-            //初始化ServerSocket对象
-            server = new ServerSocket(8080);
+            //初始化ServerSocket对象,创建绑定到特定端口的服务器套接字
+            server = new ServerSocket(webContext.port);
 
             //初始化threadPool对象,让其里面有100根线程
-            theradPool = Executors.newFixedThreadPool(100);
+            theradPool = Executors.newFixedThreadPool(webContext.maxSize);
 
         }catch (Exception e){
             e.printStackTrace();
@@ -39,7 +38,7 @@ public class WebServer {
 
         try {
             while(true) {
-                //接收客户端请求
+                //监听并接受到此套接字的连接(接收客户端请求)
                 Socket socket = server.accept();
 
                 //利用线程池执行线程类
